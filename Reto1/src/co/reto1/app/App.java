923b4ca75761e993217f4c8c9fdfd0d61fbe9544
camilo2402 <david.marin8181@gmail.com>
2021-09-30 21:06:13 -0500

    Reto1 POO

--- Reto1/src/co/reto1/app/App.java
+++ Reto1/src/co/reto1/app/App.java
@@ -3,7 +3,9 @@ package co.reto1.app;
 import co.reto1.dominio.Calculadora;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
     /*
@@ -18,13 +20,15 @@ public class App {
     private static final int RESTAR = 1;
     private static final int MULTIPLICAR = 2;
     private static final int DIVIDIR = 3;
+    private static final int AGREGAR = 0;
+    private static final int RESULTADO = 1;
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
         JCheckBox redondear = new JCheckBox("Redondear");
         do {
             int seleccion = JOptionPane.showOptionDialog(null, "Selecciona la operación que deseas realizar.", "Calculadora", 0, JOptionPane.PLAIN_MESSAGE, icono, Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir", redondear).toArray(), null);
             switch (seleccion){
                 case SUMAR:{
+                    Calculadora calculadora = new Calculadora(ingresarNumeros(seleccion));
                     double suma = calculadora.sumar();
                     if (redondear.isSelected()){
                         suma = calculadora.redondear(suma);
@@ -33,6 +37,7 @@ public class App {
                     break;
                 }
                 case RESTAR:{
+                    Calculadora calculadora = new Calculadora(ingresarNumeros(seleccion));
                     double resta = calculadora.restar();
                     if (redondear.isSelected()){
                         resta = calculadora.redondear(resta);
@@ -41,6 +46,7 @@ public class App {
                     break;
                 }
                 case MULTIPLICAR:{
+                    Calculadora calculadora = new Calculadora(ingresarNumeros(seleccion));
                     double multiplicacion = calculadora.multiplicar();
                     if (redondear.isSelected()){
                         multiplicacion = calculadora.redondear(multiplicacion);
@@ -49,6 +55,7 @@ public class App {
                     break;
                 }
                 case DIVIDIR:{
+                    Calculadora calculadora = new Calculadora(ingresarNumeros(seleccion));
                     double division = calculadora.dividir();
                     if (redondear.isSelected()){
                         division = calculadora.redondear(division);
@@ -64,6 +71,45 @@ public class App {
     }
     private static void mostrarResultado(double resultado, String titulo) {
         JOptionPane.showMessageDialog(null, "El resultado es: " + resultado, titulo, JOptionPane.INFORMATION_MESSAGE);
+    }
+    private static List<Double> ingresarNumeros(int opcion){
+        List<Double> numeros = new ArrayList<>();
+        boolean ciclo = true;
+        do{
+            try{
+                Double num = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese un numero: ", "Calculadora", JOptionPane.QUESTION_MESSAGE));
+                if (opcion == DIVIDIR && numeros.size() >= 1 && num == 0){
+                    JOptionPane.showMessageDialog(null, "Ingrese un numero válido.","Calculadora", JOptionPane.ERROR_MESSAGE);
+                    continue;
+                }
+                numeros.add(num);
+            }
+            catch(Exception error){
+                JOptionPane.showMessageDialog(null, "Ingrese un numero válido.","Calculadora", JOptionPane.ERROR_MESSAGE);
+                continue;
+            }
+            if (numeros.size()<2){
+                ciclo = true;
+            }
+            else {
+                int seleccion = JOptionPane.showOptionDialog(null, "¿Desea agregar otro número?", "Calculadora", 0, JOptionPane.WARNING_MESSAGE
+                        , null, Arrays.asList("Agregar", "Resultado").toArray(), null);
+                switch (seleccion) {
+                    case AGREGAR: {
+                        ciclo = true;
+                        break;
+                    }
+                    case RESULTADO: {
+                        ciclo = false;
+                        break;
+                    }
+                    case JOptionPane.CLOSED_OPTION: {
+                        System.exit(0);
+                    }
+                }
+            }
+        }while(ciclo);
+        return numeros;
     }
 }
 
--- Reto1/src/co/reto1/dominio/Calculadora.java
+++ Reto1/src/co/reto1/dominio/Calculadora.java
@@ -1,25 +1,53 @@
 package co.reto1.dominio;
 
+import java.text.DecimalFormat;
+import java.util.List;
+
 public class Calculadora {
-    private Suma sum = new Suma();
-    private Resta res = new Resta();
-    private Division div = new Division();
-    private Multiplicacion mul = new Multiplicacion();
-    private Redondeo numRed = new Redondeo();
+    private List<Double> numeros;
+
+    public Calculadora(List<Double> numeros) {
+        this.numeros = numeros;
+    }
+
     public double sumar(){
-        return sum.sumar();
+        double suma = 0;
+        for (double num : this.numeros) {
+            suma += num;
+        }
+        return suma;
     }
     public double restar(){
-        return res.restar();
+        double resta = 0;
+        for (double num : this.numeros) {
+            resta -= num;
+        }
+        return resta;
     }
     public double dividir(){
-        return div.dividir();
+        double division = 1;
+        int contador = 1;
+        for (double num : this.numeros) {
+            if (contador==1){
+                division = num;
+            }
+            else {
+                division /= num;
+            }
+            contador++;
+        }
+        return division;
     }
     public double multiplicar(){
-        return mul.multiplicar();
+        double multiplicacion = 1;
+        for (double num : this.numeros) {
+            multiplicacion *= num;
+        }
+        return multiplicacion;
     }
     public double redondear(double num){
-        return numRed.redondear(num);
+        DecimalFormat numRedondeado = new DecimalFormat("0.00");
+        return Double.parseDouble(numRedondeado.format(num).replace(",", "."));
     }
 
 }
